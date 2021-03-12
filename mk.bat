359b32da654ccee5aa5288dc204df6d4f588e58a
RamonUnch <74856804+RamonUnch@users.noreply.github.com>
2021-03-12 16:02:49 +0100

    Fix mk.bat for GCC 10.2

--- mk.bat
+++ mk.bat
@@ -1,6 +1,6 @@
 @set WARNINGS=-Wall -Wformat-security -Wstrict-overflow -Wsign-compare -Wclobbered -Wempty-body -Wignored-qualifiers -Wuninitialized -Wtype-limits -Woverride-init -Wduplicated-cond  -Wduplicated-branches -Wlogical-op  -Wnull-dereference
 
-@set CFLAGS=-Os -fno-stack-check -fno-stack-protector -mno-stack-arg-probe -march=i386 -mtune=i686 -mpreferred-stack-boundary=2 -foptimize-strlen -nostdlib -lkernel32 -luser32 -lmsvcrt -flto -Wl,-dynamicbase,-nxcompat -Wp,-D_FORTIFY_SOURCE=2 -s %WARNINGS%
+@set CFLAGS=-Os -fno-stack-check -fno-stack-protector -mno-stack-arg-probe -march=i386 -mtune=i686 -nostdlib -mpreferred-stack-boundary=2 -foptimize-strlen -lkernel32 -luser32 -lmsvcrt -flto -Wl,-dynamicbase,-nxcompat -s %WARNINGS% -D__USE_MINGW_ANSI_STDIO=0
 
 :: -finline-functions-called-once 
 :: -Wunused-parameter

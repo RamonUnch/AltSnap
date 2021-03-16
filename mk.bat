6fe18652f104b98ddb03d25ec51aaafc25ed8571
RamonUnch <74856804+RamonUnch@users.noreply.github.com>
2021-03-16 13:01:19 +0100

    lang and details

--- mk.bat
+++ mk.bat
@@ -1,18 +1,24 @@
-@set WARNINGS=-Wall -Wformat-security -Wstrict-overflow -Wsign-compare -Wclobbered -Wempty-body -Wignored-qualifiers -Wuninitialized -Wtype-limits -Woverride-init -Wduplicated-cond  -Wduplicated-branches -Wlogical-op  -Wnull-dereference
+@set WARNINGS=-Wall -Wformat-security -Wstrict-overflow -Wsign-compare -Wclobbered -Wempty-body -Wignored-qualifiers -Wuninitialized -Wtype-limits -Woverride-init -Wduplicated-cond -Wduplicated-branches -Wlogical-op -Wnull-dereference
 
-@set CFLAGS=-Os -fno-stack-check -fno-stack-protector -mno-stack-arg-probe -march=i386 -mtune=i686 -nostdlib -mpreferred-stack-boundary=2 -foptimize-strlen -lkernel32 -luser32 -lmsvcrt -flto -Wl,-dynamicbase,-nxcompat -s %WARNINGS% -D__USE_MINGW_ANSI_STDIO=0
+@set CFLAGS=-Os -fno-stack-check -fno-stack-protector -mno-stack-arg-probe -march=i386 -mtune=i686 -nostdlib -mpreferred-stack-boundary=2 -foptimize-strlen -lkernel32 -luser32 -lmsvcrt -flto -Wl,-dynamicbase,-nxcompat -s %WARNINGS% -D__USE_MINGW_ANSI_STDIO=0 -lgdi32
 
-:: -finline-functions-called-once 
 :: -Wunused-parameter
 
 @taskkill /IM AltDrag.exe 2> nul
 
+@ECHO WINDRES ALTDRAG.RC
 @windres altdrag.rc altdragr.o
+
+@ECHO WINDRES HOOKS.RC
 @windres hooks.rc hooksr.o
 
-gcc -o AltDrag.exe altdrag.c altdragr.o %CFLAGS% -lcomctl32 -mwindows -ladvapi32 -lshell32 -e_unfuckWinMain@0
-gcc -o hooks.dll hooks.c hooksr.o %CFLAGS% -mdll -lgdi32 -e_DllMain@12
+@ECHO CC ALTDRAG.EXE
+@gcc -o AltDrag.exe altdrag.c altdragr.o %CFLAGS% -lcomctl32 -mwindows -ladvapi32 -lshell32 -e_unfuckWinMain@0
+
+@ECHO CC HOOKS.DLL
+@gcc -o hooks.dll hooks.c hooksr.o %CFLAGS% -mdll -e_DllMain@12
 
 @set CFLAGS=
+@set WARNINGS=
 
 @start AltDrag.exe
--- zzip.bat
+++ zzip.bat
@@ -4,7 +4,7 @@
 advzipNT -a4 -i 256 AltDrag_bin.zip AltDrag.txt AltDrag.exe Hooks.dll AltDrag.ini License.txt Lang\*
 Deflopt AltDrag_bin.zip
 
-advzipNT -a4 -i 32 AltDrag_src.zip AltDrag.txt AltDrag.ini License.txt altdrag.c altdrag.rc config.c hooks.c hooks.rc languages.c languages.h resource.h tray.c unfuck.h window.rc x86.exe.manifest mk.bat media\* Lang\*
+advzipNT -a4 -i 32 AltDrag_src.zip AltDrag.txt AltDrag.ini License.txt altdrag.c altdrag.rc config.c hooks.c hooks.rc languages.c languages.h resource.h tray.c rpc.h unfuck.h window.rc x86.exe.manifest mk.bat media\* Lang\*
 
 Deflopt AltDrag_src.zip
 

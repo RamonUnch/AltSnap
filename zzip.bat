20701a401d7295fe6319d19fca54a89934575f0e
RamonUnch <74856804+RamonUnch@users.noreply.github.com>
2022-02-19 12:29:01 +0100

    Delete zzip.bat

--- zzip.bat
+++
@@ -1,3 +0,0 @@
-advzipNT -a4 -i 16 AltSnap_src.zip AltSnap.txt AltSnap.ini AltSnap.xml altsnap.nsi License.txt altsnap.c altsnap.rc config.c hooks.c hooks.rc languages.c languages.h resource.h tray.c hooks.h unfuck.h nanolibc.h window.rc AltSnap.exe.manifest makefile media\* Lang\*
-
-Deflopt AltSnap_src.zip

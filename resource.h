bb3d2a33f3b4f05fb8f06991b471d75c325b8751
RamonUnch <74856804+RamonUnch@users.noreply.github.com>
2024-05-25 09:27:23 +0200

    bump version

--- resource.h
+++ resource.h
@@ -5,8 +5,8 @@
 #define IDC_STATIC (-1)
 #endif
 
-#define VERSIONRC     1,6,3,0
-#define APP_VERSION  "1.63"
+#define VERSIONRC     1,6,4,0
+#define APP_VERSION  "1.64"
 
 #define APP_ICON                         101
 #define TRAY_OFF                         102

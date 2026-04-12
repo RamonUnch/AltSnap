71f5db3a11449cbb1d2beaff571b3ef920259c02
RamonUnch <74856804+RamonUnch@users.noreply.github.com>
2026-04-12 14:01:04 +0200

    Fix visual C++ 6.0 build

--- mkvcc6.bat
+++ mkvcc6.bat
@@ -1,7 +1,7 @@
 rc hooks.rc
 rc altsnap.rc
 
-@set DEFINES=/D "DWORD_PTR=DWORD" /D "LONG_PTR=INT_PTR" /D "ULONG_PTR=DWORD"
+@set DEFINES=/D "DWORD_PTR=DWORD" /D "LONG_PTR=INT_PTR" /D "ULONG_PTR=DWORD" /D "UNICODE" /D "_UNICODE"
 @set DEFINES= %DEFINES% /D "NO_VISTA" /D "NO_OLEAPI" /D "DECORATED_HOOKS_DLL_PROCS"
 
 cl /c /Tp altsnap.c /nologo /Ox /Oi /Os /Gy  %DEFINES%

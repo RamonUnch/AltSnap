e1baee8c355e7bbdcfbb7cab4685bfaf0f38a8ed
RamonUnch <74856804+RamonUnch@users.noreply.github.com>
2026-04-20 18:58:31 +0200

    Fix mk.bat file with a loop to kill previous AltSnap instance

--- mk.bat
+++ mk.bat
@@ -1,7 +1,11 @@
-@taskkill /IM AltSnap.exe 2> nul
-@taskkill /IM AltSnap.exe 2> nul
 
-make %1
+:loop
+@taskkill /IM AltSnap.exe 2> nul
+@if %ERRORLEVEL% EQU 0 (
+	goto :loop
+)
+
+make -j 2 %1
 
 @if !%1 == !clean GOTO FINISH
 @start AltSnap.exe
